f5cea1d96bd5bacf1cd176162b12787eb8063ce0
Vamsi <vamsi.skrishna@gmail.com>
2018-04-13 12:18:06 +0530

    Implemented put, post and delete actions
    TODO: Need to cleanup the code.

--- retailstore/control/locations.py
+++ retailstore/control/locations.py
@@ -1,3 +1,4 @@
+import falcon
 from marshmallow import fields, Schema
 from sqlalchemy import orm
 
@@ -11,13 +12,17 @@ class LocationSchema(Schema):
 
 
 class CollectionResource(BaseResource):
-
-    schema = LocationSchema(many=True)
+    get_schema = LocationSchema(many=True)
+    post_schema = LocationSchema()
 
     def on_get(self, req, resp):
         locations = self.orm_session.query(Location).all()
         req.context['result'] = locations
 
+    def on_post(self, req, resp):
+        location = Location(**req.context['json'])
+        self.orm_session.add(location)
+        self.orm_session.commit()
 
 class ItemResource(BaseResource):
 
@@ -30,3 +35,27 @@ class ItemResource(BaseResource):
             req.context['result'] = location
         except orm.exc.NoResultFound:
             raise LocationNotFound(location_id=location_id)
+
+    def on_put(self, req, resp, location_id):
+        try:
+            location = self.orm_session.query(
+                Location).filter_by(id=location_id).one()
+            location_info = req.context['json']
+            location.name = location_info['name']
+            self.orm_session.commit()
+            resp.status = falcon.HTTP_204
+            resp.location = req.path
+        except orm.exc.NoResultFound:
+            raise LocationNotFound(location_id=location_id)
+
+    def on_delete(self, req, resp, location_id):
+        try:
+            location = self.orm_session.query(
+                Location).filter_by(id=location_id).one()
+            self.orm_session.delete(location)
+            self.orm_session.commit()
+            resp.status = falcon.HTTP_202
+            resp.location = req.path
+        except orm.exc.NoResultFound:
+            raise LocationNotFound(location_id=location_id)
+        pass
